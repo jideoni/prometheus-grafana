68ecdbd782a2a0e12ed3149572615041ca7a15ef
Babajide Oni <127896618+jideoni@users.noreply.github.com>
2024-05-20 17:16:31 +0100

    Update install-grafana.sh
    Added installation command for Amazon Linux 2 AMI

--- install-grafana.sh
+++ install-grafana.sh
@@ -1,6 +1,9 @@
-sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
-sudo dpkg -i grafana_7.3.4_amd64.deb
+#sudo apt-get install -y adduser libfontconfig1
+#wget https://dl.grafana.com/oss/release/grafana_7.3.4_amd64.deb
+#sudo dpkg -i grafana_7.3.4_amd64.deb
+
+#For Amazon Linux 2 AMI
+sudo yum install -y https://dl.grafana.com/enterprise/release/grafana-enterprise-11.0.0-1.x86_64.rpm
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
